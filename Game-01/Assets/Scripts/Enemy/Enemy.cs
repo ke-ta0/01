761e861f36f5aaf7e89bc63aa1b130a1e0bbaf14
ke-ta0 <ktc25a31a0039@edu.kyoto-tech.ac.jp>
2026-09-15 21:17:21 +0900

    進捗
    攻撃、ダメージ、ノックバックの途中

--- Game-01/Assets/Scripts/Enemy/Enemy.cs
+++ Game-01/Assets/Scripts/Enemy/Enemy.cs
@@ -14,7 +14,7 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] private GameObject AttackObj;
     [SerializeField] private Transform Target;
-    [SerializeField] private float HP = 3.0f;
+    [SerializeField] private float health = 3.0f;
     [SerializeField] private float Range = 5.0f;
     [SerializeField] private float Speed = 3.0f;
     private float currentHP;
@@ -22,24 +22,24 @@ public class Enemy : MonoBehaviour
     private bool isAttacking = false;
     // 現在の行動
     private Act state;
-    
+
     void Start()
     {
-        currentHP = HP;
+        currentHP = health;
         // 最初の行動をWalkに設定
         state = Act.Walk;
     }
-    　　// Update is called once per frame
+    // Update is called once per frame
     void Update()
     {
         float dist = Vector2.Distance(transform.position, Target.position);
 
-        if(dist <= Range)
+        if (dist <= Range)
         {
             state = Act.fight;
         }
 
-        else if(currentHP <= 1)
+        else if (currentHP <= 1)
         {
             state = Act.escape;
         }
@@ -48,7 +48,7 @@ public class Enemy : MonoBehaviour
             state = Act.Walk; ;
         }
 
-        switch(state)
+        switch (state)
         {
             case Act.Walk:
                 Walk();
@@ -63,47 +63,59 @@ public class Enemy : MonoBehaviour
                 break;
         }
     }
-    void Walk()
-    {
-        //transform.Translate(Vector2.left * Speed * Time.deltaTime);
-        Debug.Log("歩いてます");
-    }
 
-    void fight()
+    public void TakeDamage(int damage)
     {
-        float dist = Vector2.Distance(transform.position, Target.position);
-        if(isAttacking)
+        currentHP -= damage;
+        Debug.Log("ダメージ受け申した");
+        if (currentHP <= 0)
         {
-            return;
-        }
-
-        Vector2 dist2 = (Target.position - transform.position).normalized;
-        // 上下移動しない
-        dist2.y = 0;
-        transform.position += (Vector3)(dist2 * Speed * Time.deltaTime);
-        if (dist <= AttackRange)
-        {
-            StartCoroutine(Attack());
+            Destroy(gameObject);
+            Debug.Log("やられたあ");
         }
     }
-    IEnumerator Attack()
-    {
-        isAttacking = true;   // 攻撃開始
-        Debug.Log("攻撃中");
+        void Walk()
+        {
+            //transform.Translate(Vector2.left * Speed * Time.deltaTime);
+            Debug.Log("歩いてます");
+        }
 
-        // 攻撃アニメーションや処理
-        yield return new WaitForSeconds(1f);
+        void fight()
+        {
+            float dist = Vector2.Distance(transform.position, Target.position);
+            if (isAttacking)
+            {
+                return;
+            }
 
-        isAttacking = false;  // 攻撃終了
+            Vector2 dist2 = (Target.position - transform.position).normalized;
+            // 上下移動しない
+            dist2.y = 0;
+            transform.position += (Vector3)(dist2 * Speed * Time.deltaTime);
+            if (dist <= AttackRange)
+            {
+                StartCoroutine(Attack());
+            }
+        }
+        IEnumerator Attack()
+        {
+            isAttacking = true;   // 攻撃開始
+            Debug.Log("攻撃中");
 
-    }
+            // 攻撃アニメーションや処理
+            yield return new WaitForSeconds(1f);
 
-    void escape()
-    {
-        Vector2 dist2 = (transform.position - Target.position).normalized;
-        // 上下移動しない
-        dist2.y = 0;
-        transform.position += (Vector3)(dist2 * Speed * Time.deltaTime);
-    }
+            isAttacking = false;  // 攻撃終了
+
+        }
+
+        void escape()
+        {
+            Vector2 dist2 = (transform.position - Target.position).normalized;
+            // 上下移動しない
+            dist2.y = 0;
+            transform.position += (Vector3)(dist2 * Speed * Time.deltaTime);
+        }
+    
 }
 
---
+++ Game-01/Assets/Scripts/Player/Attack.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] private Transform AttackPoint;
+    [SerializeField] private float AttackRange;
+    [SerializeField] private LayerMask EnemyLayer;
+    [SerializeField] private int Damage;
+    [SerializeField] private float AttackCooldown;
+    private float lastAttackTime = 0f;
+
+    void Update()
+    {
+        bool canAttack = Time.time >= lastAttackTime + AttackCooldown;
+
+        if(Input.GetKeyDown(KeyCode.V) && canAttack)
+        {
+            Attack();
+            lastAttackTime = Time.time;
+        }
+    }
+    void Attack()
+    {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(AttackPoint.position, AttackRange, EnemyLayer);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            var health = enemy.GetComponent<Enemy>();
+            if (health != null)
+            {
+                health.TakeDamage(Damage);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (AttackPoint != null)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(AttackPoint.position, AttackRange);
+        }
+    }
+}
